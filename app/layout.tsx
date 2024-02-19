7b535867cd26f419be60a910c2b7ccd11fc61b63
jaimeserranodev <jaime.serrano.dev@gmail.com>
2024-02-19 14:16:25 +0000

    Navbar finished

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
+import  Navbar  from "@/app/components/navbar/Navbar"
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
